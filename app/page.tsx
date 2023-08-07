3c6d174dd2a056416e89fec24fa5883955ded5cf
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-07 19:45:39 +0900

    최상단 page

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,14 @@
-import { demos } from '#/lib/demos'
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="space-y-8">
-      <h1 className="text-xl font-medium text-gray-300">Examples</h1>
+    <div>
+      <p>
+        next(app dir) + zustand + typesciprt + tailwindcss + pnpm +
+        tanstack-query + 서버 컴포넌트 쓰려는 노력하는 프로젝트
+      </p>
 
-      <div className="space-y-10 text-white">
-        {demos.map((section) => {
-          return (
-            <div key={section.name} className="space-y-5">
-              <div className="text-xs font-semibold uppercase tracking-wider text-gray-400">
-                {section.name}
-              </div>
-
-              <div className="grid grid-cols-1 gap-5 lg:grid-cols-2">
-                {section.items.map((item) => {
-                  return (
-                    <Link
-                      href={`/${item.slug}`}
-                      key={item.name}
-                      className="group block space-y-1.5 rounded-lg bg-gray-900 px-5 py-3 hover:bg-gray-800"
-                    >
-                      <div className="font-medium text-gray-200 group-hover:text-gray-50">
-                        {item.name}
-                      </div>
-
-                      {item.description ? (
-                        <div className="text-sm text-gray-400 line-clamp-3 group-hover:text-gray-300">
-                          {item.description}
-                        </div>
-                      ) : null}
-                    </Link>
-                  )
-                })}
-              </div>
-            </div>
-          )
-        })}
-      </div>
+      <Link href="/signup">로그인 링크</Link>
     </div>
-  )
+  );
 }
