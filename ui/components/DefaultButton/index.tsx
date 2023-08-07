b18e9f74457c92a09aeb4aa8e0932046883ac9fa
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-07 19:38:01 +0900

    기본 버튼 컴포넌트

---
+++ ui/components/DefaultButton/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./DefaultButton.module.css";
+import { ComponentPropsWithoutRef } from "react";
+
+export type PickButtonTagProps = ComponentPropsWithoutRef<"button">;
+
+type Props = {
+  children: React.ReactNode;
+  disabled: boolean;
+} & PickButtonTagProps;
+
+export const DefaultButton = ({ children, disabled, ...props }: Props) => {
+  return (
+    <button
+      data-disabled={disabled}
+      disabled={disabled}
+      className={styles.root}
+      onClick={props.onClick}
+    >
+      {children}
+    </button>
+  );
+};
