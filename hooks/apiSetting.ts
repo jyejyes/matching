4ac391a77afd19726dedf2aa3b9d0f2a116ba51a
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-08 20:55:15 +0900

    api 설정 파일

---
+++ hooks/apiSetting.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { AxiosError } from "axios";
+import { getCookie } from "#/utils/Cookie/Cookie";
+
+export const BASE_URL = "http://175.45.202.103";
+axios.defaults.withCredentials = true;
+
+//일단 쿠키 써봄
+const token = getCookie("token");
+
+export const apiClient = axios.create({
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
+
+apiClient.interceptors.request.use(
+  (config: any) => {
+    config.withCredentials = true;
+
+    config.headers["Authorization"] = `Bearer ${token}`;
+
+    return config;
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error);
+  }
+);
