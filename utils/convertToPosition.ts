9b0c81ad7c46f935014899a926cc6f75afc1e7a6
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-08 20:49:17 +0900

    convert-position util 함수 추가

--- utils/convertToPosition.ts
+++ utils/convertToPosition.ts
@@ -1,12 +1,12 @@
 export const convertToPosition = (devName: string) => {
   switch (devName) {
     case "BACK_END":
-      return "백엔드 개발";
+      return "백엔드 개발자";
     case "FRONT_END":
-      return "프론트엔드 개발";
+      return "프론트엔드 개발자";
     case "PM_PO":
       return "PM/PO";
     case "DESIGNER":
-      return "디자인";
+      return "디자이너";
   }
 };
