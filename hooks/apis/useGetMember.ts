58d68fd5c96cacedf080379545b99e78bc684076
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-08 20:59:05 +0900

    유저 정보 불러오기 api hook

---
+++ hooks/apis/useGetMember.ts
@@ -0,0 +1,34 @@
+import { apiClient } from "#/hooks/apiSetting";
+import { matchSchema, userSchema } from "#/types/schema/schema";
+import { z } from "zod";
+import { useQuery } from "@tanstack/react-query";
+
+const MemberSchema = z.object({
+  id: userSchema.id,
+  userProviderId: matchSchema.userProviderId,
+  username: userSchema.username,
+  position: userSchema.position,
+  RegistrationSource: matchSchema.registrationSource,
+  intro: userSchema.intro,
+  imgUrl: userSchema.imgUrl,
+  interest: userSchema.interest,
+  skill: userSchema.skill,
+});
+
+type MemberType = z.infer<typeof MemberSchema>;
+const member = async () => {
+  const response = await apiClient.get("/member");
+
+  return MemberSchema.parse(response.data.data);
+};
+
+export const useGetMember = () => {
+  const {
+    isLoading,
+    isSuccess,
+    isError,
+    data = {} as MemberType,
+  } = useQuery(["member"], member);
+
+  return { isLoading, isSuccess, isError, data };
+};
