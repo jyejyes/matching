d7c85837ca17d0b5bb9d3a02d81645beab378b17
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-08 20:56:27 +0900

    탈퇴, 로그아웃 모달

---
+++ app/setting/_components/DeleteModal.tsx
@@ -0,0 +1,37 @@
+"use client";
+import { Modal } from "#/ui/components/modal/Modal";
+import useModalControl from "#/app/modalControl.state";
+import { useRouter } from "next/navigation";
+import routerPaths from "#/utils/routerPaths";
+
+export const DeleteModal = () => {
+  const { isDeleteModalOpen, updateIsDeleteModalOpen } = useModalControl();
+
+  const { push } = useRouter();
+  const handleClickDelete = () => {
+    // 탈퇴 로직
+
+    push(routerPaths.signup());
+  };
+
+  const handleClickCancel = () => {
+    updateIsDeleteModalOpen(!isDeleteModalOpen);
+  };
+
+  return (
+    <Modal
+      onConfirm={handleClickDelete}
+      confirmText="회원 탈퇴"
+      onCancel={handleClickCancel}
+      cancelText="취소"
+    >
+      <p className="text-[18px] font-bold mb-2">회원탈퇴</p>
+
+      <p className="text-[14px]">
+        정말 회원탈퇴하시겠어요?
+        <br />
+        모든 데이터가 사라지며, 다시 복구할 수 없습니다.
+      </p>
+    </Modal>
+  );
+};
---
+++ app/setting/_components/LogoutModal.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { Modal } from "#/ui/components/modal/Modal";
+import useModalControl from "#/app/modalControl.state";
+import { useRouter } from "next/navigation";
+import routerPaths from "#/utils/routerPaths";
+
+export const LogoutModal = () => {
+  const { isLogoutModalOpen, updateIsLogoutModalOpen } = useModalControl();
+
+  const { push } = useRouter();
+  const handleClickLogout = () => {
+    // 로그아웃 로직
+
+    push(routerPaths.signup());
+  };
+
+  const handleClickCancel = () => {
+    updateIsLogoutModalOpen(!isLogoutModalOpen);
+  };
+
+  return (
+    <Modal
+      onConfirm={handleClickLogout}
+      confirmText="로그아웃"
+      onCancel={handleClickCancel}
+      cancelText="취소"
+    >
+      <p className="text-[18px] font-bold mb-2">로그아웃</p>
+
+      <p className="text-[14px]">정말 로그아웃 하시겠습니까?</p>
+    </Modal>
+  );
+};
