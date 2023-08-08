0227634779d48c5f503c37726dcdcdf28fca7227
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-08 20:55:02 +0900

    스키마 정의

---
+++ types/schema/schema.ts
@@ -0,0 +1,59 @@
+//회원가입 관련
+import { z } from "zod";
+
+export const authUserSchema = {
+  id: z.number(),
+  name: z.string(),
+  email: z.string(),
+  image: z.string(),
+};
+
+export const tokenSchema = {
+  expires: z.string().optional(),
+  accessToken: z.string().optional(),
+
+  jwtToken: z.string(),
+};
+
+export const signupSchema = {
+  skill: z.string(),
+
+  avatarImgName: z.string(),
+  avatarImgUrl: z.string(),
+
+  //login
+};
+
+export const userSchema = {
+  id: z.number(),
+  imgUrl: z.string(),
+  interest: z.array(z.string()),
+  intro: z.string(),
+  position: z.enum(["BACK_END", "FRONT_END", "PM_PO", "DESIGNER", ""]),
+  skill: z.array(z.string()),
+  username: z.string(),
+};
+
+export const TimeSchema = {
+  time: z.string(),
+};
+
+export const matchSchema = {
+  registrationSource: z.string().optional(),
+  id: z.number(),
+  imgUrl: userSchema.imgUrl,
+  interest: userSchema.interest,
+  intro: userSchema.intro,
+  position: userSchema.position,
+  skill: userSchema.skill,
+  userProviderId: z.string(),
+  username: userSchema.username,
+  matchTime: TimeSchema.time,
+};
+export const matchingLikeSchema = {
+  like: z.boolean(),
+};
+
+export const chatSchema = {
+  messageRoomId: z.number(),
+};
