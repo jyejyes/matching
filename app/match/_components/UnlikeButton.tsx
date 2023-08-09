29dbea7aa04aea0df9217977a2df85939bd3e442
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-09 23:03:16 +0900

    like, unlike버튼 수정

--- app/match/_components/UnlikeButton.tsx
+++ app/match/_components/UnlikeButton.tsx
@@ -1,12 +1,18 @@
 import { PickButtonTagProps } from "#/ui/components/DefaultButton";
 import Image from "next/image";
+import clsx from "clsx";
 
-type Props = PickButtonTagProps;
+type Props = {
+  status: "match" | "people";
+} & PickButtonTagProps;
 
-export const UnlikeButton = ({ ...props }: Props) => {
+export const UnlikeButton = ({ status, ...props }: Props) => {
   return (
     <button
-      className="w-[60px] h-[60px] rounded-full bg-white flex-center shadow-lg"
+      className={clsx(
+        "w-[60px] h-[60px] rounded-full  flex-center ",
+        status === "people" ? "bg-white shadow-lg" : "bg-gray1"
+      )}
       onClick={props.onClick}
     >
       <Image
