8975a4506bf27f29153233da7d9224ddf0126096
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-09 23:02:59 +0900

    not found 파일 추가

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
+
 export default function NotFound() {
   return (
-    <div>
-      <h2 className="text-lg font-bold">Not Found</h2>
+    <div className="relative bg-white w-full h-full flex flex-col items-center justify-center">
+      <h2 className="text-subtitle font-extrabold mb-2">
+        페이지를 찾을 수 없어요.
+      </h2>
+      <p className="font-bold text-[17px] text-gray5 mb-[35px]">
+        다시 시도해주세요.
+      </p>
 
-      <p className="text-sm">Could not find requested resource</p>
+      <Image
+        src={"/images/img-notfound.png"}
+        alt={"not-found"}
+        width={138}
+        height={170}
+      />
     </div>
   );
 }
