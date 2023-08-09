9c2185e61c08c471ba1055b9274fbbbcc9fef761
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-09 23:06:47 +0900

    vercel mix content 해결되는지 테스트

--- hooks/apiSetting.ts
+++ hooks/apiSetting.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { AxiosError } from "axios";
 import { getCookie } from "#/utils/Cookie/Cookie";
 
-export const BASE_URL = "http://175.45.202.103";
+export const BASE_URL = "https://175.45.202.103";
 axios.defaults.withCredentials = true;
 
 //일단 쿠키 써봄
@@ -11,6 +11,9 @@ const token = getCookie("token");
 export const apiClient = axios.create({
   baseURL: BASE_URL,
   withCredentials: true,
+  httpsAgent: new (require("https").Agent)({
+    rejectUnauthorized: false, //허가되지 않은 인증을 reject하지 않겠다!
+  }),
 });
 
 apiClient.interceptors.request.use(
