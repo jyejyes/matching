40193e90b11c3e1b9dad0d91a5e268a189487f16
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-10 12:03:09 +0900

    url 수정

--- app/setting/_components/Terms.tsx
+++ app/setting/_components/Terms.tsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
 
 export default function Terms() {
+  const termsOfUse =
+    "https://solyourock.notion.site/7053ddfba6404b73bf1df3a3c4070e78?pvs=4";
+  const privacyPolicy =
+    "https://solyourock.notion.site/891788ec05434a0f912ce3ef16ce55cb?pvs=4";
+
   return (
     <>
       <p className="text-[13px] text-gray6">기타</p>
 
       <div>
         <a
-          href={
-            "https://www.notion.so/solyourock/7053ddfba6404b73bf1df3a3c4070e78"
-          }
+          href={termsOfUse}
           target={"_blank"}
           className="w-full py-[14px] bg-white flex items-center gap-[11px]"
         >
@@ -22,9 +25,7 @@ export default function Terms() {
           이용약관
         </a>
         <a
-          href={
-            "https://www.notion.so/solyourock/891788ec05434a0f912ce3ef16ce55cb"
-          }
+          href={privacyPolicy}
           target={"_blank"}
           className="w-full py-[14px] bg-white flex items-center gap-[11px]"
         >
