679459d13b5ce0d5169b5f5bdf2f6994110da8b8
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-10 14:35:28 +0900

    사람 상세 페이지 버튼 추가

--- app/people/[id]/page.tsx
+++ app/people/[id]/page.tsx
@@ -1,11 +1,25 @@
 import { UserInfo } from "#/app/people/[id]/_components/UserInfo";
 import { UserImage } from "#/app/people/[id]/_components/UserImage";
 import Buttons from "#/app/people/[id]/_components/Buttons";
-import { useParams } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+import routerPaths from "#/utils/routerPaths";
 
 export default function Page() {
   return (
     <div className="w-full h-full relative flex flex-col justify-between">
+      <Link
+        href={routerPaths.match()}
+        className="rounded-full bg-white bg-opacity-60 absolute top-4 left-4 w-[45px] h-[45px] flex-center shadow-md"
+      >
+        <Image
+          src={"/images/signup/ic-backward.svg"}
+          alt={"back"}
+          width={24}
+          height={24}
+        />
+      </Link>
+
       <div>
         <UserImage />
 
