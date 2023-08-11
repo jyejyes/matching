60a5b66449b6249c1eeee21f290929539dd945e8
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-11 13:43:43 +0900

    인피니티 이미지 추가

--- app/signup/_components/InfiniteImages.tsx
+++ app/signup/_components/InfiniteImages.tsx
@@ -1,23 +1,62 @@
-import Image from "next/image";
+"use client";
 import React from "react";
+import { motion } from "framer-motion";
 
+const slideVariants1 = {
+  start: { x: "0%" },
+  end: {
+    x: "-100%",
+    transition: { duration: 20, ease: "linear", repeat: Infinity },
+  },
+};
+
+const slideVariants2 = {
+  start: { x: "-100%" },
+  end: {
+    x: "0%",
+    transition: { duration: 20, ease: "linear", repeat: Infinity },
+  },
+};
 export const InfiniteImages = () => {
   return (
-    <div className="mb-[71px]">
-      <Image
-        src={"/images/signup/img-main1@x2.png"}
-        alt={"top"}
-        width={1290}
-        height={120}
-        className="mb-8"
-      />
+    <div className="mb-[71px] flex flex-col gap-7">
+      <div className="min-h-[120px] w-[1290px] flex gap-5 overflow-hidden">
+        <motion.img
+          src="/images/signup/img-main1@x2.png"
+          alt="Infinite Slider"
+          initial="start"
+          animate="end"
+          variants={slideVariants1}
+          className="h-[120px]"
+        />
+        <motion.img
+          src="/images/signup/img-main1@x2.png"
+          alt="Infinite Slider"
+          initial="start"
+          animate="end"
+          variants={slideVariants1}
+          className="h-[120px]"
+        />
+      </div>
 
-      <Image
-        src={"/images/signup/img-main2@x2.png"}
-        alt={"bottom"}
-        width={1290}
-        height={120}
-      />
+      <div className="min-h-[120px] w-[1290px] flex gap-5 overflow-hidden">
+        <motion.img
+          src="/images/signup/img-main2@x2.png"
+          alt="Infinite Slider"
+          initial="start"
+          animate="end"
+          variants={slideVariants2}
+          className="h-[120px]"
+        />
+        <motion.img
+          src="/images/signup/img-main2@x2.png"
+          alt="Infinite Slider"
+          initial="start"
+          animate="end"
+          variants={slideVariants2}
+          className="h-[120px] w-[2580px]"
+        />
+      </div>
     </div>
   );
 };
