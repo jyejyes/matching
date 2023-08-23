0d495854bcc1459593be49352db9000056d5a20a
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-23 18:45:44 +0900

    콘솔로그 삭제

--- app/_components/LibraryWrapper.tsx
+++ app/_components/LibraryWrapper.tsx
@@ -40,8 +40,6 @@ export default function LibraryWrapper({ children }: Props) {
         data: { type, content },
       } = parsedRes;
 
-      console.log(parsedRes);
-
       if (type === "MATCH") {
         updateMatchingSuccessInfo(content);
 
