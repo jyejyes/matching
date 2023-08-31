9cc8b840ba0fb74d79593af89cabc045b99b50cf
jyejyes <jh100m1@jellypage.com>
2023-08-31 22:57:20 +0900

    signup 으로 이동

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import routerPaths from "#/utils/routerPaths";
 
 export default function Page() {
+  const { push } = useRouter();
+
+  useEffect(() => {
+    push(routerPaths.signup());
+  }, []);
   return (
     <div>
-      <p>
-        next(app dir) + zustand + typesciprt + tailwindcss + pnpm +
-        tanstack-query + 서버 컴포넌트 쓰려는 노력하는 프로젝트
-      </p>
+      <p>tecky</p>
 
       <Link href="/signup">로그인 링크</Link>
     </div>
