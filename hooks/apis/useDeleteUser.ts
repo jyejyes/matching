a0aac6679c0085ad2dda750b430f12f594d61bd4
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-09-04 15:44:22 +0900

    -

--- hooks/apis/useDeleteUser.ts
+++ hooks/apis/useDeleteUser.ts
@@ -1,6 +1,7 @@
 import { apiClient } from "#/hooks/apiSetting";
 import { useMutation } from "@tanstack/react-query";
 import LocalStorage from "#/utils/LocalStorage";
+import useModalControl from "#/app/modalControl.state";
 
 const deleteUser = async () => {
   const response = await apiClient.delete("/member");
@@ -9,9 +10,12 @@ const deleteUser = async () => {
 };
 
 export const useDeleteUser = () => {
+  const { updateIsDeleteModalOpen } = useModalControl();
+
   return useMutation(deleteUser, {
     onSuccess: () => {
       LocalStorage.clear();
+      updateIsDeleteModalOpen(false);
     },
   });
 };
